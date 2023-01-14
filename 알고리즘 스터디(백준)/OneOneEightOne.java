9e62a4b62b68ccbdc1b813975a9101d871dfed22
yunsbob <w124564@naver.com>
2023-01-15 00:30:42 +0900

    word sort

--- 알고리즘 스터디(백준)/OneOneEightOne.java
+++ 알고리즘 스터디(백준)/OneOneEightOne.java
@@ -1,8 +1,42 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 
 public class OneOneEightOne {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        HashSet<String> hs = new HashSet<>();
+		
+        for (int i = 0; i < n; i++) {
+			hs.add(br.readLine());
+		}
+		
+        List<String> list = new ArrayList<>(hs);
+
+		Collections.sort(list, new Comparator<String>() {
+			public int compare(String s1, String s2) {
+				if (s1.length() == s2.length()) {
+					return s1.compareTo(s2);
+				} 
+				else {
+					return s1.length() - s2.length();
+				}
+			}
+		});
+		
+        for(String result : list){
+            bw.write(result + "\n");
+        }
+        bw.flush();
+	}
 }
