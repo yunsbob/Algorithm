aa3aa846fa0f1e48178994bc3f4fa538880c07a6
yunsbob <w124564@naver.com>
2023-01-15 15:02:47 +0900

    binomial coefficient

---
+++ 매일 알고리즘 하나씩/OneOneZeroFiveZero.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class OneOneZeroFiveZero {
+    public static int Factorial(int n){
+        if(n <= 1) return 1;
+        else return n * Factorial(n-1);
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        bw.write((Factorial(n) / (Factorial(n - k) * Factorial(k))) + "\n");
+        bw.flush();
+    }
+}
