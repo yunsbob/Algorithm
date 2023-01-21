1f8a8c924f02ff2acd9ca1a25fe99fa3d7662691
yunsbob <w124564@naver.com>
2023-01-21 12:54:32 +0900

    .

--- 매일 알고리즘 하나씩/재귀/Gcd.java
+++ 매일 알고리즘 하나씩/재귀/Gcd.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class TwoSixZeroNine{
+public class Gcd{
     public static int gcd(int num1, int num2){
         if(num2 == 0) return num1;
         else return gcd(num2, num1 % num2);
