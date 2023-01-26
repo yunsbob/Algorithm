a7df1469768064932fdd17c2e2b107644f3ced70
yunsbob <w124564@naver.com>
2023-01-27 00:10:51 +0900

    Hash Map

--- 매일 알고리즘 하나씩/그외/OneSixTwoZero.java
+++ 매일 알고리즘 하나씩/그외/OneSixTwoZero.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Main {
+public class OneSixTwoZero {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
