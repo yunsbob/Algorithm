14779640db2fcb6e326a66d5c894d22b5ea2e5b8
yunsbob <w124564@naver.com>
2023-01-28 20:57:47 +0900

    segment tree

--- 매일 알고리즘 하나씩/자료구조/세그먼트트리/SegmentTree.java
+++ 매일 알고리즘 하나씩/자료구조/세그먼트트리/SegmentTree.java
@@ -1,6 +1,5 @@
 public class SegmentTree {
-    static int[] arr;
-    static int[] tree;
+    static int[] arr, tree;
 
     public static int init(int start, int end, int node){
         if(start == end){
@@ -11,10 +10,10 @@ public class SegmentTree {
     }
 
     public static int sum(int start, int end, int node, int left, int right){
-        if(left > end || right < start){ // 범위를 벗어난 경우
+        if(left > end || right < start){
             return 0;
         }
-        if(left <= start && end <= right){ // 범위에 들어온 경우
+        if(left <= start && end <= right){
             return tree[node];
         }
         int mid = (start + end) / 2;
@@ -22,7 +21,7 @@ public class SegmentTree {
     }
 
     public static void update(int start, int end, int node, int updateIndex, int updateValue){
-        if(updateIndex < start || updateIndex > end){ // 범위를 벗어난 경우
+        if(updateIndex < start || updateIndex > end){
             return ;
         }
         tree[node] += updateValue;
@@ -37,14 +36,12 @@ public class SegmentTree {
         for(int i = 0; i < 10; i++){
             arr[i] = i + 1;
         }
-        tree = new int[arr.length * 4]; // 트리 크기는 배열의 4배
-        int num = arr.length - 1; // num = 배열의 마지막 인덱스
+        tree = new int[arr.length * 4];
+        int num = arr.length - 1;
         
-        // 노드만 1부터 시작하고 나머지는 배열의 인덱스로
         init(0, num, 1);
         System.out.println(sum(0, num, 1, 0, 9));
         System.out.println(sum(0, num, 1, 0, 0));
-        // 0번 인덱스를 +5로 변경
         update(0, num, 1, 0, 5);
         System.out.println(sum(0, num, 1, 0, 9));
         System.out.println(sum(0, num, 1, 0, 0));
