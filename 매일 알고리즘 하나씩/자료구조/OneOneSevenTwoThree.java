d9f8f777a66bcb60b066ebff075ea0bb8cdf65e8
yunsbob <w124564@naver.com>
2023-01-29 19:50:34 +0900

    Hash Set

---
+++ 매일 알고리즘 하나씩/자료구조/OneOneSevenTwoThree.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class OneOneSevenTwoThree {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int M = Integer.parseInt(br.readLine());
+        HashSet<Integer> hs = new HashSet<>();
+        
+        for(int i = 0; i < M; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String order = st.nextToken();
+            if(order.equals("add")){
+                hs.add(Integer.parseInt(st.nextToken()));
+            }
+            else if(order.equals("remove")){
+                int num = Integer.parseInt(st.nextToken());
+                if(hs.contains(num)){
+                    hs.remove(num);
+                }
+            }
+            else if(order.equals("check")){
+                if(hs.contains(Integer.parseInt(st.nextToken()))){
+                    sb.append("1\n");
+                }
+                else{
+                    sb.append("0\n");
+                }
+            }
+            else if(order.equals("toggle")){
+                int num = Integer.parseInt(st.nextToken());
+                if(hs.contains(num)){
+                    hs.remove(num);
+                }
+                else{
+                    hs.add(num);
+                }
+            }
+            else if(order.equals("all")){
+                for(int j = 1; j <= 20; j++){
+                    if(hs.contains(j)){
+                        hs.remove(j);
+                    }
+                    else{
+                        hs.add(j);
+                    }
+                }
+            }
+            else if(order.equals("empty")){
+                hs = new HashSet<>();
+            }
+        }
+        System.out.println(sb);
+    }
+}
