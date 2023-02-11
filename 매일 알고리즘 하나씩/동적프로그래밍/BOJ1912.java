cc31fcf37fcd1aa73803cea117c37b539b4aa2ef
yunsbob <w124564@naver.com>
2023-02-11 11:59:28 +0900

    DP

---
+++ 매일 알고리즘 하나씩/동적프로그래밍/BOJ1912.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1912 {
+    static Integer[] dp;
+    static int[] A;
+    
+    public static int sum(int index, int num){
+        if(index == -1){
+            return num;
+        }
+        if(dp[index] != null ){
+            return num + dp[index];
+        }
+        return Math.max(sum(index-1, num + A[index]), num + A[index]);
+    }
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        dp = new Integer[N];
+        A = new int[N];
+        int result = -1001;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+            dp[i] = sum(i, 0);
+            if(result < dp[i]){
+                result = dp[i];
+            }
+        }
+        sb.append(result);
+        System.out.println(sb);
+    }
+}
