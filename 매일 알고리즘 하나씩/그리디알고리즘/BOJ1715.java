fd3e0c77effa3e01472321524649d4d5cecfe62c
YunsBob <w124564@naver.com>
2023-03-11 11:24:40 +0900

    add

---
+++ 매일 알고리즘 하나씩/그리디알고리즘/BOJ1715.java
@@ -0,0 +1,31 @@
+package everyday.Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class BOJ1715 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Long> pq = new PriorityQueue<>();
+		for(int i = 0; i < N; i++) {
+			pq.offer(Long.parseLong(br.readLine()));
+		}
+		
+		long sum = 0, n, m;	
+
+		while(pq.size() > 1) {
+			n = pq.poll();
+			m = pq.poll();
+			sum += n + m;
+			pq.offer(n + m);
+		}
+
+		System.out.println(sum);
+	}
+
+}
