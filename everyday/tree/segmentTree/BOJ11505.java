573c3122a40f4baea7daaa45ff28424ac03fc27d
YunsBob <w124564@naver.com>
2023-07-06 12:18:45 +0900

    Add

---
+++ everyday/tree/segmentTree/BOJ11505.java
@@ -0,0 +1,73 @@
+package everyday.tree.segmentTree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ11505 {
+    static long[] arr, tree;
+    static long MOD = 1000000007;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        arr = new long[N];
+        tree = new long[N * 4];
+
+        for(int i = 0; i < N; i++){
+            arr[i] = Long.parseLong(br.readLine());
+        }
+
+        init(0, N - 1, 1);
+
+        for(int i = 0; i < M + K; i++){
+            st = new StringTokenizer(br.readLine());
+            int order = Integer.parseInt(st.nextToken());
+            if(order == 1){
+                int index = Integer.parseInt(st.nextToken());
+                long value = Long.parseLong(st.nextToken());
+                update(0, N - 1, 1, index - 1, value);
+                arr[index - 1] = value;
+            }
+            else if(order == 2){
+                sb.append(mul(0, N - 1, 1, Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1) + "\n");
+            }
+        }
+        System.out.println(sb);
+    }
+
+    public static long init(int start, int end, int node){
+        if(start == end){
+            return tree[node] = arr[start];
+        }
+        int mid = (start + end) / 2;
+        return tree[node] = (init(start, mid, node * 2) * init(mid + 1, end, node * 2 + 1)) % MOD;
+    }
+
+    public static long mul(int start, int end, int node, int left, int right){
+        if(left > end || right < start){
+            return 1;
+        }
+        if(left <= start && end <= right){
+            return tree[node];
+        }
+        int mid = (start + end) / 2;
+        return (mul(start, mid, node * 2, left, right) * mul(mid + 1, end, node * 2 + 1, left, right)) % MOD;
+    }
+
+    public static long update(int start, int end, int node, int updateIndex, long updateValue){
+        if(updateIndex < start || updateIndex > end){
+            return tree[node];
+        }
+        tree[node] = updateValue;
+        if(start == end) return tree[node];
+        int mid = (start + end) / 2;
+        return tree[node] = (update(start, mid, node * 2, updateIndex, updateValue) * update(mid + 1, end, node * 2 + 1, updateIndex, updateValue)) % MOD;
+    }
+}
