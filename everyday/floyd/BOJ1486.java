7d1f40c342bc79ff09e7bc1edc2fd6b8d9ef76a5
YunsBob <w124564@naver.com>
2023-07-09 22:18:18 +0900

    Perf

--- everyday/floyd/BOJ1486.java
+++ everyday/floyd/BOJ1486.java
@@ -6,10 +6,10 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class BOJ1486 {
-    private static int N, M, T;
+    private static int N, M, T, D;
     private static char[][] map;
-    private static int[][][][] dist;
     private static int[] height;
+    private static int[][] distUp;
 
     private static class XY {
         int x, y, cnt;
@@ -28,22 +28,17 @@ public class BOJ1486 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         T = Integer.parseInt(st.nextToken());
+        D = Integer.parseInt(st.nextToken());
         map = new char[N][M];
-        dist = new int[N][M][N][M];
+        distUp = new int[N][M];
+        int[][] distDown = new int[N][M];
 
-        int D = Integer.parseInt(st.nextToken());
         for (int i = 0; i < N; i++) {
             map[i] = br.readLine().toCharArray();
         }
 
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                for (int k = 0; k < N; k++) {
-                    for (int l = 0; l < M; l++) {
-                        dist[i][j][k][l] = D + 1;
-                    }
-                }
-            }
+            Arrays.fill(distUp[i], D + 1);
         }
 
         height = new int['z' + 1];
@@ -54,30 +49,35 @@ public class BOJ1486 {
             height[i] = i - 'a' + 26;
         }
 
+        dijkstra(0, 0, distUp);
+        int res = height[map[0][0]];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                dijkstra(i, j);
-            }
-        }
-
-        int res = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (dist[0][0][i][j] + dist[i][j][0][0] <= D) res = Math.max(res, height[map[i][j]]);
+                if (i == 0 && j == 0) continue;
+                if (distUp[i][j] + i + j > D) continue;
+                if (height[map[i][j]] < res) continue;
+                for (int k = 0; k < N; k++) {
+                    Arrays.fill(distDown[k], D + 1);
+                }
+                dijkstra(i, j, distDown);
+                if (distUp[i][j] + distDown[0][0] <= D) {
+                    res = height[map[i][j]];
+                }
             }
         }
         System.out.println(res);
     }
 
-    private static void dijkstra(int sx, int sy) {
+    private static void dijkstra(int sx, int sy, int[][] dist) {
         int[][] dr = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
         PriorityQueue<XY> q = new PriorityQueue<>(((o1, o2) -> o1.cnt - o2.cnt));
-        dist[sx][sy][sx][sy] = 0;
+        dist[sx][sy] = 0;
         q.offer(new XY(sx, sy, 0));
         while (!q.isEmpty()) {
             XY now = q.poll();
             if (sx != 0 && sy != 0 && now.x == 0 && now.y == 0) return;
+            if (distUp[sx][sy] + dist[now.x][now.y] > D) return;
             for (int i = 0; i < 4; i++) {
                 int nx = now.x + dr[i][0];
                 int ny = now.y + dr[i][1];
@@ -85,19 +85,16 @@ public class BOJ1486 {
 
                 int h = Math.abs(height[map[nx][ny]] - height[map[now.x][now.y]]);
                 if (h <= T) {
+                    int dis;
                     if (height[map[now.x][now.y]] < height[map[nx][ny]]) {
-                        int dis = now.cnt + h * h;
-                        if (dist[sx][sy][nx][ny] > dis) {
-                            q.offer(new XY(nx, ny, dis));
-                            dist[sx][sy][nx][ny] = dis;
-                        }
+                        dis = now.cnt + h * h;
                     }
                     else {
-                        int dis = now.cnt + 1;
-                        if (dist[sx][sy][nx][ny] > dis) {
-                            q.offer(new XY(nx, ny, dis));
-                            dist[sx][sy][nx][ny] = dis;
-                        }
+                        dis = now.cnt + 1;
+                    }
+                    if (dist[nx][ny] > dis) {
+                        q.offer(new XY(nx, ny, dis));
+                        dist[nx][ny] = dis;
                     }
                 }
             }
