ab71e3a3f12e03cdc156cf328772b021aac5eb60
YunsBob <w124564@naver.com>
2023-12-08 17:56:24 +0900

    Add

---
+++ everyday/dp/BOJ11052.java
@@ -0,0 +1,30 @@
+package everyday.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ11052 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N + 1];
+		int[] dp = new int[N + 1];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= i; j++) {
+				dp[i] = Math.max(dp[i], dp[i - j] + arr[j]);
+			}
+		}
+
+		System.out.println(dp[N]);
+	}
+}
