41f99b1a527b875c31d1e6c4262e9b499c604094
Minjae Yun <w124564@naver.com>
2024-01-20 16:55:01 +0900

    [Gold V] Title: 동전 2, Time: 108 ms, Memory: 11872 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		int[] dp = new int[k + 1];
+
+		for(int i = 0 ; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		Arrays.fill(dp,10001);
+		dp[0] = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = arr[i]; j <= k; j++) {
+				dp[j] = Math.min(dp[j], dp[j - arr[i]] + 1);
+			}
+		}
+
+		System.out.println(dp[k] == 10001 ? -1 : dp[k]);
+	}
+}
