8b84569441b7cd674f21af64aec737268e20db4b
Minjae Yun <w124564@naver.com>
2024-02-03 19:53:22 +0900

    [Bronze V] Title: 합, Time: 84 ms, Memory: 12148 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.kt
+++ 백준/Bronze/8393. 합/합.kt
@@ -1,5 +1,9 @@
-fun main() = with(System.`in`.bufferedReader()){
-    var n = readLine().toInt()
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
     var sum = 0
     for (i in 1..n) sum += i
     println(sum)
