375b07c7ea426032d41c23e62c6e66e45744886e
Minjae Yun <w124564@naver.com>
2024-02-05 23:02:42 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 88 ms, Memory: 12156 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.kt
@@ -0,0 +1,8 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toLong()
+    println("${n * (n - 1) * (n - 2) / 6}\n3")
+}
