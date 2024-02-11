5854eef0dd2eff92c91571f18aa16b50afb28637
Minjae Yun <w124564@naver.com>
2024-02-11 22:55:42 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.01 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(n: Int): Int {
+        for(i in 2 .. n) {
+            if(n % i == 1) {
+                return i
+            }
+        }
+        
+        return -1
+    }
+}
