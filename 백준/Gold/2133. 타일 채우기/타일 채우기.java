8a8791170904955d5c4bddb202b4dcd8dc0b8bd0
Minjae Yun <w124564@naver.com>
2024-02-12 22:04:33 +0900

    [Gold IV] Title: 타일 채우기, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		if (N % 2 == 0) {
+			int[] dp = new int[N + 1];
+			dp[0] = 1;
+			dp[2] = 3;
+			for (int i = 4; i <= N; i += 2) {
+				dp[i] = dp[i - 2] * 3;
+
+				for (int j = 0; j <= i - 4; j += 2) {
+					dp[i] += dp[j] * 2;
+				}
+			}
+
+			System.out.println(dp[N]);
+		} else {
+			System.out.println(0);
+		}
+	}
+}
