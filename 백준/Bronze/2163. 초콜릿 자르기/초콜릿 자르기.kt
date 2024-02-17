67de0505ac75778688442620b42c629f6334f297
Minjae Yun <w124564@naver.com>
2024-02-17 13:16:33 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 84 ms, Memory: 11900 KB -BaekjoonHub

--- 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.kt
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.kt
@@ -1,13 +1,14 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.lang.Integer.parseInt
 import java.util.StringTokenizer
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val st = StringTokenizer(br.readLine())
 
-    val n = st.nextToken().toInt()
-    val m = st.nextToken().toInt()
+    val n = parseInt(st.nextToken())
+    val m = parseInt(st.nextToken())
     
     println(n * m - 1)
 }
