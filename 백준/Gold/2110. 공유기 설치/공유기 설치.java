0d6475340fe37efa931de7cff469cf0fcf2bf0fc
Minjae Yun <w124564@naver.com>
2024-02-24 16:38:59 +0900

    [Gold IV] Title: 공유기 설치, Time: 272 ms, Memory: 28428 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+
+		int min = 1, max = arr[N - 1];
+		while (min <= max) {
+			int mid = (min + max) / 2;
+
+			int cnt = 1;
+			int last = arr[0];
+			for (int i = 1; i < N; i++) {
+				int now = arr[i];
+
+				if (now - last >= mid) {
+					cnt++;
+					last = now;
+				}
+			}
+
+			if (cnt < C) {
+				max = mid - 1;
+			} else {
+				min = mid + 1;
+			}
+		}
+
+		System.out.println(min - 1);
+	}
+}
