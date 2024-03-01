528d212390a141f0026cfd87efb5ce5e3486beaa
Minjae Yun <w124564@naver.com>
2024-03-01 13:28:19 +0900

    [Silver I] Title: 신입 사원, Time: 780 ms, Memory: 297436 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,22 +13,21 @@ public class Main {
 		int T = Integer.parseInt(br.readLine());
 		for (int i = 0; i < T; i++) {
 			int N = Integer.parseInt(br.readLine());
-			int[][] arr = new int[N][2];
+			int[] arr = new int[N + 1];
 			for (int j = 0; j < N; j++) {
 				st = new StringTokenizer(br.readLine());
 
-				arr[j][0] = Integer.parseInt(st.nextToken());
-				arr[j][1] = Integer.parseInt(st.nextToken());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				arr[a] = b;
 			}
 
-			Arrays.sort(arr, (o1, o2) -> { return o1[0] - o2[0]; });
-
 			int res = 1;
-			int cut = arr[0][1];
-			for (int j = 1; j < N; j++) {
-				if (arr[j][1] < cut) {
+			int cut = arr[1];
+			for (int j = 2; j <= N; j++) {
+				if (arr[j] < cut) {
 					res++;
-					cut = arr[j][1];
+					cut = arr[j];
 				}
 			}
 
