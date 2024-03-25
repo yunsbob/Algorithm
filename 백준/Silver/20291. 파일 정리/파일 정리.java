04455380693e0aa5165e5c92cacbecea70629e85
Minjae Yun <w124564@naver.com>
2024-03-25 16:25:53 +0900

    [Silver III] Title: 파일 정리, Time: 640 ms, Memory: 82288 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		Map<String, Integer> treeMap = new TreeMap<>();
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), ".");
+			st.nextToken();
+			String extension = st.nextToken();
+			treeMap.put(extension, treeMap.getOrDefault(extension, 0) + 1);
+		}
+
+		for (String extension : treeMap.keySet()) {
+			sb.append(extension).append(' ').append(treeMap.get(extension)).append('\n');
+		}
+
+		System.out.println(sb);
+	}
+}
