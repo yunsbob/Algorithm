0f71af8206c8baadea44e2540066fea1d2aff4b7
Minjae Yun <w124564@naver.com>
2024-04-18 18:22:44 +0900

    [Gold V] Title: 노드사이의 거리, Time: 172 ms, Memory: 16820 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -1,12 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int N;
-	private static int[][] arr;
+	private static int N, res;
+	private static List<List<Node>> list = new ArrayList<>();
+
+	private static class Node {
+		int n, d;
+
+		public Node(int n, int d) {
+			this.n = n;
+			this.d = d;
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,41 +25,42 @@ public class Main {
 
 		N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		arr = new int[N + 1][N + 1];
-		for (int i = 1; i <= N; i++) {
-			Arrays.fill(arr[i], 1_000_001);
+		for (int i = 0; i <= N; i++) {
+			list.add(new ArrayList<>());
 		}
 		for (int i = 1; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
-			arr[a][b] = w;
-			arr[b][a] = w;
+			list.get(a).add(new Node(b, w));
+			list.get(b).add(new Node(a, w));
 		}
-
-		floyd();
 
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			sb.append(arr[a][b]).append('\n');
+			res = 0;
+			dfs(0, a, 0, b);
+			sb.append(res).append('\n');
 		}
 
 		System.out.println(sb);
 	}
 
-	private static void floyd() {
-		for (int k = 1; k <= N; k++) {
-			for (int i = 1; i <= N; i++) {
-				if (i == k)
+	private static void dfs(int prev, int now, int dist, int end) {
+		if (now == end) {
+			res = dist;
+		} else {
+			for (Node next : list.get(now)) {
+				if (prev == next.n)
 					continue;
 
-				for (int j = 1; j <= N; j++) {
-					if (arr[i][j] > arr[i][k] + arr[k][j])
-						arr[i][j] = arr[i][k] + arr[k][j];
-				}
+				dfs(now, next.n, dist + next.d, end);
+
+				if (res != 0)
+					return;
 			}
 		}
 	}
