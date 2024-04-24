ffcd86941aa31b510e2d743cdf8cea9e84048b28
Minjae Yun <w124564@naver.com>
2024-04-24 15:32:35 +0900

    [Gold IV] Title: 오큰수, Time: 772 ms, Memory: 196872 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Deque<Integer> stack = new ArrayDeque<>();
+		for (int i = 0; i < N; i++) {
+			while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+				arr[stack.pop()] = arr[i];
+			}
+
+			stack.push(i);
+		}
+
+		while (!stack.isEmpty()) {
+			arr[stack.pop()] = -1;
+		}
+
+		for (int i = 0; i < N; i++) {
+			sb.append(arr[i]).append(' ');
+		}
+
+		System.out.println(sb);
+	}
+}
