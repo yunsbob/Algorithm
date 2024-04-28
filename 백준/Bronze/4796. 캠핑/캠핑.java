54b1ae87f6ece5504a212c8fa65bad56750b34d3
Minjae Yun <w124564@naver.com>
2024-04-28 20:16:29 +0900

    [Bronze I] Title: 캠핑, Time: 92 ms, Memory: 11992 KB -BaekjoonHub

---
+++ 백준/Bronze/4796. 캠핑/캠핑.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int t = 1;
+		while (true) {
+			st = new StringTokenizer(br.readLine());
+			int L = Integer.parseInt(st.nextToken());
+			int P = Integer.parseInt(st.nextToken());
+			int V = Integer.parseInt(st.nextToken());
+
+			if (L == 0 && P == 0 && V == 0)
+				break;
+
+			int res = V / P * L;
+			if (V % P > L) {
+				res += L;
+			} else {
+				res += V % P;
+			}
+
+			sb.append("Case ").append(t++).append(": ").append(res).append('\n');
+		}
+
+		System.out.println(sb);
+	}
+}
