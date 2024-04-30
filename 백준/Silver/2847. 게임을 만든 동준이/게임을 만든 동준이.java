bff3dad49c7d091dfb5f4f583cb3946cffabb9fd
Minjae Yun <w124564@naver.com>
2024-04-30 15:12:35 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 80 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		int res = 0;
+		for (int i = N - 2; i >= 0; i--) {
+			if (arr[i] >= arr[i + 1]) {
+				res += arr[i] - arr[i + 1] + 1;
+				arr[i] = arr[i + 1] - 1;
+			}
+		}
+
+		System.out.println(res);
+	}
+}
