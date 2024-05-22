0585fbef670f7b592560ed435b10754ac6be411e
Minjae Yun <w124564@naver.com>
2024-05-22 15:34:23 +0900

    [Silver II] Title: 상자넣기, Time: 108 ms, Memory: 12032 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int[] dp = new int[n];
+
+		int res = 0;
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			dp[i] = 1;
+
+			for (int j = 0; j < i; j++) {
+				if (arr[i] > arr[j]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+
+			res = Math.max(res, dp[i]);
+		}
+
+		System.out.println(res);
+	}
+}
