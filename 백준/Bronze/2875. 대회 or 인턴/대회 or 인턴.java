946689720f798403bf50c2ca5115fad0af3bff32
Minjae Yun <w124564@naver.com>
2024-06-18 07:47:36 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		int res = 0;
+		while (N >= 2 && M >= 1 && N + M - K >= 3) {
+			res++;
+			N -= 2;
+			M--;
+		}
+
+		System.out.println(res);
+	}
+}
