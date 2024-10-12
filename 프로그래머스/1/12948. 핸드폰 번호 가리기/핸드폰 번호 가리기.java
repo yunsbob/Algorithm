dd6c6245e86bc2bc87c06288f6d7ab30480a2086
Minjae Yun <w124564@naver.com>
2024-10-12 21:52:35 +0900

    Update 핸드폰 번호 가리기.java

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -2,7 +2,7 @@ class Solution {
     public String solution(String phone_number) {
         String answer = "";
         int i = 0;
-        for (i = 0; i < phone_number.length() - 4; i++) {
+        for (; i < phone_number.length() - 4; i++) {
             answer += "*";
         }
         
