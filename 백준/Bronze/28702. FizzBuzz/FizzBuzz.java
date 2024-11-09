ba93cbb427a634d0cc281fc5ec0e71194df2e1bc
Minjae Yun <w124564@naver.com>
2024-11-09 16:00:51 +0900

    [Bronze I] Title: FizzBuzz, Time: 68 ms, Memory: 11632 KB -BaekjoonHub

---
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = 0;
+		for (int i = 0; i < 3; i++) {
+			String str = br.readLine();
+			if (str.matches("^\\d+$")) {
+				n = Integer.parseInt(str);
+				n += 3 - i;
+				break;
+			}
+		}
+
+		if (n % 3 == 0 && n % 5 == 0) {
+			System.out.println("FizzBuzz");
+		} else if (n % 3 == 0) {
+			System.out.println("Fizz");
+		} else if (n % 5 == 0) {
+			System.out.println("Buzz");
+		} else {
+			System.out.println(n);
+		}
+	}
+}
