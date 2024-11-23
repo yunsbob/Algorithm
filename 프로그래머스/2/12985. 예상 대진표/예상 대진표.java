fefbd64bed3fd32e35da37e21729fa3d3ebea42a
Minjae Yun <w124564@naver.com>
2024-11-23 20:31:35 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        
+        while (a != b) {
+            a = (a + 1) >> 1;
+            b = (b + 1) >> 1;
+            answer++;
+        }
+
+        return answer;
+    }
+}
