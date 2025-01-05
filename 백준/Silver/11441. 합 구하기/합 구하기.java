8c6b1eee60f3300d4f93b7049c29468ce01787c2
Minjae Yun <w124564@naver.com>
2025-01-05 20:40:23 +0900

    [Silver III] Title: 합 구하기, Time: 424 ms, Memory: 56960 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N + 1];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+		}
+
+		int M = Integer.parseInt(br.readLine());
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			sb.append(arr[e] - arr[s - 1]).append('\n');
+		}
+
+		System.out.println(sb);
+	}
+}
