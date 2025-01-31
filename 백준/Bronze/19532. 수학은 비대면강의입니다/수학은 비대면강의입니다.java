2744deed70f2245fc3f7941571d2b486e9ec8dd9
Minjae Yun <w124564@naver.com>
2025-01-31 16:30:07 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 104 ms, Memory: 12196 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		int f = Integer.parseInt(st.nextToken());
+
+		A: for (int x = -999; x <= 999; x++) {
+			for (int y = -999; y <= 999; y++) {
+				if (a * x + b * y == c && d * x + e * y == f) {
+					sb.append(x).append(' ').append(y);
+					break A;
+				}
+			}
+		}
+
+		System.out.println(sb);
+	}
+}
