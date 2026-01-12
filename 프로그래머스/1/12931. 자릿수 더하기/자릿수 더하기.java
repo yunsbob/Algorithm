940c3488c4b39fe7cf1203643d068124b17d526a
Minjae Yun <w124564@naver.com>
2026-01-12 22:55:09 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 92.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int res = 0;
+        while(n > 0) {
+            res += n % 10;
+            n /= 10;
+        }
+
+        return res;
+    }
+}
